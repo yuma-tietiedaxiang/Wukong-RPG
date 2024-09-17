6f084a02bb708b6517924dce8e635e9cbd3e0edd
SaltedReed <u7726159@anu.edu.au>
2024-09-17 20:47:38 +1000

    add ci test

---
+++ src/test/java/comp2120/ass3/PlayerTest.java
@@ -0,0 +1,12 @@
+package comp2120.ass3;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PlayerTest {
+    @Test
+    public void test() {
+        // for dev only
+        Assert.assertTrue(true);
+    }
+}
