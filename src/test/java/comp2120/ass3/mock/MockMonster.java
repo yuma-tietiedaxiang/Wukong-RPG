ae3696b04f45777d2323aa28583bd187ad190295
SaltedReed <u7726159@anu.edu.au>
2024-09-23 19:03:40 +1000

    modified tests

---
+++ src/test/java/comp2120/ass3/mock/MockMonster.java
@@ -0,0 +1,50 @@
+package comp2120.ass3.mock;
+
+import comp2120.ass3.IMonster;
+
+public class MockMonster implements IMonster {
+    @Override
+    public int getX() {
+        return 0;
+    }
+
+    @Override
+    public int getY() {
+        return 0;
+    }
+
+    @Override
+    public void move(int offsetX, int offsetY) {
+
+    }
+
+    @Override
+    public int getMaxHealth() {
+        return 0;
+    }
+
+    @Override
+    public int getHealth() {
+        return 0;
+    }
+
+    @Override
+    public void reduceHealth(int amount) {
+
+    }
+
+    @Override
+    public int attack() {
+        return 0;
+    }
+
+    @Override
+    public int getDamage() {
+        return 0;
+    }
+
+    @Override
+    public int getDefense() {
+        return 0;
+    }
+}
