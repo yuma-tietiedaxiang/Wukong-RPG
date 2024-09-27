fbc7d76a8204f1cf52eda6e5cd017b65a4103037
u7602081 <jun.zhu@anu.edu.au>
2024-09-27 18:20:32 +1000

    add javadoc and comment for Interfaces

--- src/main/java/comp2120/ass3/IMonster.java
+++ src/main/java/comp2120/ass3/IMonster.java
@@ -1,7 +1,32 @@
 package comp2120.ass3;
 
 /**
- * Represent a monster (enemy of the player)
+ * Represents a monster, which is an enemy of the player in the game.
+ * This interface extends the {@link ICombatObject} interface,
+ * indicating that a monster can participate in combat scenarios with the player
+ * and other entities implementing the {@link ICombatObject} interface.
+ *
+ * A monster has basic combat capabilities, such as attacking, moving,
+ * and taking damage, as defined in the {@link ICombatObject} interface.
+ * Additional attributes and behaviors specific to monsters can be added
+ * in classes that implement this interface.
+ *
+ * Implementing classes might include specific monster types like {@link Monster},
+ * providing concrete details about their health, attack damage, defense,
+ * and other combat-related attributes.
+ *
+ * By using this interface, the game engine can manage all types of monsters
+ * in a uniform way, regardless of their specific implementations.
+ *
+ * This interface serves as a marker for all monster entities in the game.
+ *
+ * @see ICombatObject
+ * @see Monster
+ * @see Player
+ *
+ * @author Jun Zhu
  */
 public interface IMonster extends ICombatObject {
+    // This interface inherits all methods from ICombatObject,
+    // making it a specialized type of combat object.
 }
--- src/main/java/comp2120/ass3/IPlayer.java
+++ src/main/java/comp2120/ass3/IPlayer.java
@@ -1,8 +1,37 @@
 package comp2120.ass3;
 
 /**
- * The player
+ * Represents the player in the game, extending the {@link ICombatObject} interface,
+ * indicating that the player can participate in combat scenarios.
+ *
+ * A player has the ability to fight, move, and take damage like other combat objects in the game,
+ * but also possesses additional attributes and behaviors that are unique to the player character.
+ *
+ * The {@link IPlayer} interface adds a new method {@code getGold()} to retrieve the amount of gold
+ * the player currently has, representing the player's currency or resources that can be used
+ * for various in-game transactions like buying items or healing.
+ *
+ * This interface can be implemented by various player character classes, allowing for different
+ * player types with potentially different combat abilities and resource management strategies.
+ *
+ * The game engine can utilize this interface to manage the player’s interactions, combat behavior,
+ * and resource management in a consistent manner.
+ *
+ * @see ICombatObject
+ * @see Player
+ * @see Monster
+ * @see GameEngine
+ * @see IMonster
+ * @see Inventory
+ *
+ * @author Jun Zhu
  */
 public interface IPlayer extends ICombatObject {
+    /**
+     * Retrieves the amount of gold the player currently possesses.
+     * Gold can be used for purchasing items, healing, and other in-game activities.
+     *
+     * @return the current amount of gold the player has.
+     */
     int getGold();
 }
